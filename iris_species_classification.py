fffbb1f25c6d5ca01ed1d59d64bb1dc31f0ceaa4
Rahul Singh <31428749+srahuliitb@users.noreply.github.com>
2021-06-19 03:04:41 +0530

    Changing the import statement of matplotlib.pyplot

--- iris_species_classification.py
+++ iris_species_classification.py
@@ -1,7 +1,7 @@
 # Importing the necessary libraries.
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import streamlit as st
 from sklearn.model_selection import train_test_split
